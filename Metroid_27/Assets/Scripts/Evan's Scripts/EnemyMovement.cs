fa2ab5a48ea0a95826a865f48571d5601c567bc1
Ech4189 <ech4189@gmail.com>
2024-11-07 15:17:21 -0800

    Enemy Movement Script
    Movement is complete - has not been tested yet

--- Metroid_27/Assets/Scripts/Evan's Scripts/EnemyMovement.cs
+++ Metroid_27/Assets/Scripts/Evan's Scripts/EnemyMovement.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Name: Evan Henderson
+ * Date: 11/7/2024
+ * Function: Handles the movement of enemies
+ */
+
 public class EnemyMovement : MonoBehaviour
 {
+    public GameObject leftPoint;
+    public GameObject rightPoint;
+    private Vector3 leftPos;
+    private Vector3 rightPos;
+    public float speed;
+    public bool movingRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        leftPos = leftPoint.transform.position;
+        rightPos = rightPoint.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyMove();
+    }
+
+    private void EnemyMove()
+    {
+        if (movingRight)
+        {
+            if (transform.position.x >= rightPos.x)
+            {
+                movingRight = false;
+            }
+            else
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (transform.position.x <= leftPos.x)
+            {
+                movingRight = true;
+            }
+            else
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+        }
     }
 }
