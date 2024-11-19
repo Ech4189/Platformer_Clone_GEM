c7e3d3ea13982a80cbbd9ad856d650f3d87b6b91
spookspoops <gr.lintao@rocketmail.com>
2024-11-18 22:50:36 -0800

    levels 1-4

--- Metroid_27/Assets/Scripts/Gabe's Script/Portal.cs
+++ Metroid_27/Assets/Scripts/Gabe's Script/Portal.cs
@@ -6,11 +6,13 @@ public class Portal : MonoBehaviour
 {
     public GameObject TeleportPoint;
     public GameObject TeleportPoint1b;
+    public GameObject TeleportPoint2;
 
     private void OnTriggerEnter(Collider other)
     {
         other.transform.position = TeleportPoint.transform.position;
         other.transform.position = TeleportPoint1b.transform.position;
+        other.transform.position = TeleportPoint2.transform.position;
     }
 
 }
