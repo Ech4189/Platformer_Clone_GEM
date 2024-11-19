026bda9c80575e8c7953fd5bfa1be25760791ed5
Ech4189 <ech4189@gmail.com>
2024-11-18 20:28:56 -0800

    Hard Enemy Prefab Update

---
+++ Metroid_27/Assets/Scripts/Evan's Scripts/HeavyBulletPickup.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Name: Evan Henderson
+ * Date: 11/18/2024
+ * Function: Handles the Heavy Bullet pick-up item
+ */
+
+public class HeavyBulletPickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
